70f0313e874ae7ce80666eb5ff9b56f2b65d099a
Alvin <alvin.alvin@sirclo.co.id>
2019-09-26 21:30:20 +0700

    Changed R

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "cashAR/routers"
+	_ "cashAr/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"cashAR/controllers"
+	"cashAr/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
